c1f6a6e1974388a24909b0639cf683dd3afeaee2
Will <williamlancelotsanchez@gmail.com>
2022-06-29 19:43:24 -0400

    CreateListing xml and onMutate function created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -87,12 +87,13 @@ function Profile() {
               type="text"
               id="email"
               className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
-              disabled="true"
+              disabled={true}
               value={email}
               onChange={onChange}
             />
           </form>
         </div>
+
         <Link to="/create-listing" className="createListing">
           <img src={homeIcon} alt="home" />
           <p>Sell or rent your home</p>
